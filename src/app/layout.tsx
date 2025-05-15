e54b7654eb8f0565f60395a72cc69b51d4e315fd
Mitchell Dawkins II <mitchell.dawkinsjr@gmail.com>
2025-05-15 14:14:36 -0400

    default to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,7 +69,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${inter.className} min-h-screen flex flex-col`}>
         <ThemeProvider 
           attribute="class" 
-          defaultTheme="light" 
+          defaultTheme="dark" 
           enableSystem={false}
           disableTransitionOnChange
         >
