cf78c7a7b50ddd3c4854151dd1fd48d6ec3c7cab
Mitchell Dawkins II <mitchell.dawkinsjr@gmail.com>
2025-06-16 13:11:12 -0400

    updating resume/about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -178,13 +178,13 @@ export default function AboutPage() {
 
         <div className="space-y-8">
             {/* DFF */}
-            <div className="border-l-4 border-blue-600 pl-4 py-1">
+            {/* <div className="border-l-4 border-blue-600 pl-4 py-1">
             <div className="flex flex-col md:flex-row md:items-center justify-between mb-2">
                 <h3 className="text-xl font-bold text-gray-900 dark:text-white">Vice President</h3>
                 <div className="text-gray-600 dark:text-gray-300">May 2023 - Present</div>
             </div>
             <div className="text-lg text-blue-600 dark:text-blue-400 mb-2">Dawkins Family Foundation Inc.</div>
-            </div>
+            </div> */}
 
             {/* Dropbox */}
             <div className="border-l-4 border-blue-600 pl-4 py-1">
